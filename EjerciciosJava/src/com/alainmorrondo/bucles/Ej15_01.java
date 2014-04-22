6e04ca83e4ab32a63028e905a0c2f0b647c0ec18
Alain Morrondo <alain.morrondo@gmail.com>
2014-04-22 12:19:28 +0200

    Construir un programa que simule el juego de la adivinanza de un número. El ordenador debe generar un número aleatorio entre 1 y 100 y el usuario tiene cinco oportunidades para acertarlo. Después de cada intento el programa debe indicarle al usuario si el número introducido por él es mayor, menor o igual al número a adivinar, y el número de intentos restantes.
    Nota: para generar el valor aleatorio puede emplearse la sentencia:
    
    int x  = (int) (100*Math.random()+1);

---
+++ EjerciciosJava/src/com/alainmorrondo/bucles/Ej15_01.java
@@ -0,0 +1,19 @@
+package com.alainmorrondo.bucles;
+
+public class Ej15_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+//Construir un programa que simule el juego de la adivinanza de un número. El ordenador debe generar un número aleatorio entre 1 y 100 y el usuario tiene cinco oportunidades para acertarlo. Después de cada intento el programa debe indicarle al usuario si el número introducido por él es mayor, menor o igual al número a adivinar, y el número de intentos restantes.
+//
+//Nota: para generar el valor aleatorio puede emplearse la sentencia:
+//
+//int x  = (int) (100*Math.random()+1);
